45e6c332490dde6c1fd0a45d48a3f2d406d4f3d7
kiran215 <kiran.sadaye@gmail.com>
2023-12-11 23:40:00 +0530

    time handling

--- 05mytime/main.go
+++ 05mytime/main.go
@@ -15,6 +15,7 @@ func main() {
 	fmt.Println(presentTime.Format("01-02-2006 15:04:05 Monday"))
 
 	createdDate := time.Date(2020, time.April, 5, 23, 23, 0, 0, time.Local)
+	fmt.Println(createdDate)
 	fmt.Println(createdDate.Format("01-02-2006 Monday"))
 
 }
